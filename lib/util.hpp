7191bc152bcbbef56cbdaf3836794478014fe810
bmistree <bmistree@stanford.edu>
2013-06-16 23:07:10 -0700

    Added query key to utils.

--- lib/util.hpp
+++ lib/util.hpp
@@ -5,12 +5,22 @@
 #include <boost/regex.hpp>
 #include <string>
 #include <iomanip>
+#include <vector>
+
+#include "../deps/lodepng/lodepng.h"
 
 #define FLOAT_WIDTH_PRECISION 20
-#define QUERY_DATA_SIZE 5
+#define IMAGE_WIDTH 200
+#define IMAGE_HEIGHT 200
+#define RGBA_PIXEL_BYTES 4
+// The total amount of data that we're passing each way should be the number of
+// pixels in the image (image_width*image_height), multiplied by the amount of
+// data required for each pixel.
+#define QUERY_DATA_SIZE IMAGE_WIDTH*IMAGE_HEIGHT*RGBA_PIXEL_BYTES
 
 // how long each message from volume servers is
-#define QUERY_RESPONSE_MESSAGE_LENGTH  FLOAT_WIDTH_PRECISION + QUERY_DATA_SIZE + 1
+#define QUERY_RESPONSE_MESSAGE_LENGTH  2*FLOAT_WIDTH_PRECISION + QUERY_DATA_SIZE + 2
+
 
 typedef uint PortNum;
 typedef uint QueryKey;
@@ -30,6 +40,8 @@ struct Query
     }
     ~Query() {}
 
+    // used to distinguish what data set are for
+    QueryKey query_key;
     // used to distinguish which query replying to.
     QueryCounter query_counter;
     // position of the camera.
@@ -38,12 +50,14 @@ struct Query
 
     bool operator== (const Query& q)
     {
-        return (query_counter == q.query_counter);
+        return ((query_counter == q.query_counter) &&
+            (query_key == q.query_key));
     }
 
     bool operator!= (const Query& q)
     {
-        return (query_counter != q.query_counter);
+        return ((query_counter != q.query_counter) ||
+            (query_key != q.query_key));
     }
     
     
@@ -51,6 +65,7 @@ struct Query
     {
         std::ostringstream str_stream;
         to_serialize_to = "";
+        str_stream << std::setw(FLOAT_WIDTH_PRECISION) << query_key << " ";
         str_stream << std::setw(FLOAT_WIDTH_PRECISION) << query_counter << " ";
         str_stream << std::setw(FLOAT_WIDTH_PRECISION) << x << " ";
         str_stream << std::setw(FLOAT_WIDTH_PRECISION) << y << " ";
@@ -62,7 +77,7 @@ struct Query
     static bool deserialize(Query& q, std::string& msg)
     {
         boost::regex regex(
-            "(\\d+)\\s+(\\d+[.]*\\d+)\\s+(\\d+[.]*\\d+)\\s+(\\d+[.]*\\d+)");
+            "(\\d+)\\s+(\\d+)\\s+(\\d+[.]*\\d*)\\s+(\\d+[.]*\\d*)\\s+(\\d+[.]*\\d*)");
 
         std::string::iterator to_search_from, end;
         to_search_from = msg.begin();
@@ -71,16 +86,18 @@ struct Query
 
         if (boost::regex_search(to_search_from,end,matches,regex))
         {
-            q.query_counter = atoi(
+            q.query_key = atoi(
                 std::string(matches[1].first,matches[1].second).c_str());
-            q.x = atof(
+            q.query_counter = atoi(
                 std::string(matches[2].first,matches[2].second).c_str());
-            q.y = atof(
+            q.x = atof(
                 std::string(matches[3].first,matches[3].second).c_str());
-            q.z = atof(
+            q.y = atof(
                 std::string(matches[4].first,matches[4].second).c_str());
+            q.z = atof(
+                std::string(matches[5].first,matches[5].second).c_str());
 
-            msg = std::string(matches[4].second,msg.end());
+            msg = std::string(matches[5].second,msg.end());
             return true;
         }
         return false;
@@ -99,7 +116,8 @@ struct QueryResponse
     {
         std::ostringstream str_stream;
         to_serialize_to = "";
-        str_stream << std::setw(FLOAT_WIDTH_PRECISION) << query_counter << " ";        
+        str_stream << std::setw(FLOAT_WIDTH_PRECISION) << query_key << " ";        
+        str_stream << std::setw(FLOAT_WIDTH_PRECISION) << query_counter << " ";
         to_serialize_to = str_stream.str();
         to_serialize_to.append(query_data, QUERY_DATA_SIZE);
     }
@@ -109,10 +127,13 @@ struct QueryResponse
         if (msg.size() < QUERY_RESPONSE_MESSAGE_LENGTH)
             return false;
 
-        q.query_counter = atof(msg.substr(0,FLOAT_WIDTH_PRECISION).c_str());
+        q.query_key = atoi(msg.substr(0,FLOAT_WIDTH_PRECISION).c_str());
+        q.query_counter = atoi(
+            msg.substr(FLOAT_WIDTH_PRECISION + 1, 2*FLOAT_WIDTH_PRECISION).c_str());
+        
         memcpy(
             q.query_data,
-            msg.substr(FLOAT_WIDTH_PRECISION +1,QUERY_DATA_SIZE).c_str(),
+            msg.substr(2*FLOAT_WIDTH_PRECISION +1,QUERY_DATA_SIZE).c_str(),
             QUERY_DATA_SIZE);
 
         msg = msg.substr(QUERY_RESPONSE_MESSAGE_LENGTH );
@@ -125,7 +146,18 @@ struct QueryResponse
         for (int index = 0; index < QUERY_DATA_SIZE; ++index)
             query_data[index] += to_merge_with.query_data[index];
     }
-    
+
+    bool to_png(std::vector<unsigned char>& png_dat)
+    {
+        std::vector<unsigned char> bmp_dat;
+        for (int index = 0; index < QUERY_DATA_SIZE; ++index)
+            bmp_dat.push_back(query_data[index]);
+
+        unsigned error = lodepng::encode(png_dat, bmp_dat, IMAGE_WIDTH, IMAGE_HEIGHT);
+        return error;
+    }
+
+    QueryKey query_key;
     QueryCounter query_counter;
     char query_data [QUERY_DATA_SIZE];
 };
